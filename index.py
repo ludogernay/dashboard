5af21c20b57e47e7b830b18787c0d49dbdb4d97d
ludogernay <ludovic.gernay@ynov.com>
2024-05-06 17:21:20 +0200

    [+] all games home

--- index.py
+++ index.py
@@ -1,17 +1,54 @@
 import cgi
-import data_processing as dp
+import pandas as pd
 form = cgi.FieldStorage()
 print("Content-type: text/html; charset=utf-8\n")
+
+df = pd.read_csv("videogames.csv")
+
+
 html = """<!DOCTYPE html>
 <head>
-    <title>DashBoard</title>
+    <title>Mon programme</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+        h1 {
+            text-align: center;
+        }
+        .container {
+            display: flex;
+            flex-wrap: wrap;
+            padding: 1rem;
+            margin: 0 auto;
+            color: white;
+            height: auto;
+            min-height: 60vh;
+            width: 80%;
+            border: solid 2px;
+            border-color: white;
+            border-radius: 7px;
+            gap: 0.5rem;
+        .game {
+            background-color: #333;
+            color: white;
+            padding: 1rem;
+            border-radius: 7px;
+            border: solid 2px;
+            border-color: white;
+            width: 30%;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <div>
-        <h1>DashBoard</h1>
-    </div>
+    <h1>Gaming Tracker</h1>
 </body>
 </html>
 """
 print(html)
-print(dp.select_between_csv_line(dp.read_csv_files("videogames.csv"), "Sales",29000000, 300000000))
+print("<div class='container'>")
+for i in df['Title'].tolist():
+    print(f"<div class=\"game\">{i}</div>")
+print("</div>")
